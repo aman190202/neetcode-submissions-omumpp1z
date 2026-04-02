fb3ae839fa36dea44d7899e602d2944e36284e4c
Aman Agarwal <agarwalaman190202@gmail.com>
2026-04-02 12:27:36 -0400

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        // vector<unordered_set<char>> rows(9);
+        // vector<unordered_set<char>> cols(9);
+        // vector<unordered_set<char>> boxes(9);
+        int rows[9] = {};
+        int cols[9] = {};
+        int boxes[9] = {};
+
+        for(int i = 0 ; i < 9 ; i++)
+        {
+            for( int j = 0 ; j< 9 ; j++)
+            {
+                if(board[i][j]=='.')
+                    continue;
+
+                int element = 1 << board[i][j];
+                int box_index = (i/3)*3 + (j/3);
+            
+
+                if(rows[i] & element || cols[j] & element || boxes[box_index] & element)
+                    return false;
+                    
+                rows[i] |= element;
+                cols[j] |= element;
+                boxes[box_index] |= element;
+            }
+        }
+        return true;
+    }
+};
